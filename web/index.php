1e56ea61866603e997c3045b126c07f0689a291a
nexik <tomek@sabaki.pl>
2014-01-24 21:56:21 +0100

    improvements in front controller

--- web/index.php
+++ web/index.php
@@ -1,14 +1,21 @@
 <?php
+// few const paths to convienience
 define('ROOT_PATH', realpath(__DIR__  . '/..'));
 define('APP_PATH', ROOT_PATH . '/app');
 define('CONFIG_PATH', APP_PATH . '/config');
 
 try {
+    // application bootstrap configuration
     include_once(APP_PATH . '/bootstrap.php');
 
-    $di =
-    $app = new \Phalcon\Mvc\Application(new \Landingi\Landend\DI\BasicDI());
+    // building dependency injection container for appliaction
+    $factory = new \Nest\DI\Factory();
+    $di = $diFactory->build('mvc');
+
+    // starting application
+    $app = new \Phalcon\Mvc\Application($di);
     echo $app->handle()->getContent();
 } catch (\Exception $e) {
+    // something went really wrong but we have nice page for our users
     include_once(APP_PATH . '/views/_errors/500/handler.php');
 }
