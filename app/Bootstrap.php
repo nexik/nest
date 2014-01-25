3ef077cdc3c24c880e4fc1e7eebc801aac21aade
nexik <tomek@sabaki.pl>
2014-01-25 13:11:19 +0100

    Clean up web/index.php

---
+++ app/Bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part Phalcon Nest (Phalcon SOLID bootstrap project for RAD)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT
+ */
+namespace App;
+
+use Nest\Application\Http as Application;
+
+/**
+ * App\Bootstrap
+ *
+ * This is Application configuration class
+ *
+ * @author  Tomasz Ślązok <tomek@sabaki.pl>
+ */
+class Bootstrap extends Application
+{
+    public function configure()
+    {
+
+    }
+}
--- web/index.php
+++ web/index.php
@@ -1,22 +1,10 @@
 <?php
-// few const paths to convienience
-define('WEB_PATH', __DIR__);
-define('ROOT_PATH', realpath(__DIR__  . '/..'));
-define('APP_PATH', ROOT_PATH . '/app');
-define('CONFIG_PATH', APP_PATH . '/config');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-try {
-    // application bootstrap configuration
-    include_once(APP_PATH . '/bootstrap.php');
+include_once(ROOT_PATH . '/src/Nest/Application/Http.php');
+include_once(ROOT_PATH . '/app/Bootstrap.php');
 
-    // building dependency injection container for appliaction
-    $factory = new \Nest\DI\Factory();
-    $di = $diFactory->build('mvc');
-
-    // starting application
-    $app = new \Phalcon\Mvc\Application($di);
-    echo $app->handle()->getContent();
-} catch (\Exception $e) {
-    // something went really wrong but we have nice page for our users
-    include_once(APP_PATH . '/views/_errors/500/handler.php');
-}
+$inProduction = getenv('IN_PRODUCTION') ?: true;
+$app = new App\Bootstrap($inProduction);
+$app->run();
